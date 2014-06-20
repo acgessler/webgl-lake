43a3c151e8dbbf348fc18d9c5a05129fc95c1eb8
Alexander Gessler <alexander.gessler@gmx.net>
2014-06-20 14:21:39 +0200

    Tweak water settings.

--- src/lib.js
+++ src/lib.js
@@ -118,7 +118,7 @@ var get_prototype_water_material = (function() {
 					medea.TEXTURE_FLAG_CLAMP_TO_EDGE,
 					// Only one channel is required
 					medea.TEXTURE_FORMAT_LUM),
-			spec_color_shininess : [0.65, 0.65, 0.7, 8.0],
+			spec_color_shininess : [0.65, 0.65, 0.7, 16.0],
 			inv_terrain_map_dim: 1.0 / TERRAIN_PLANE_WIDTH
 		}
 	);
